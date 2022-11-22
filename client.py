ae186434aca2231dab9c348221d2f5bc03f1a2f2
LFG.clan <108435214+lastforgod@users.noreply.github.com>
2022-11-22 19:47:52 +0330

    client

---
+++ client.py
@@ -0,0 +1,11 @@
+
+import binascii
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5 import Crypto.Random
+class Client:
+def __init__(self):
+random = Crypto.Random.new().read self._private_key = RSA.generate(1024, random) self._public_key = self._private_key.publickey() self._signer = PKCS1_v1_5.new(self._private_key)
+@property
+def identity(self):
+return binascii.hexlify(self._public_key.exportKey(format='DER')).decode('ascii') Ali = Client()
+print (Ali.identity)
